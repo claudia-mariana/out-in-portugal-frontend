a4440329e518a1e249487d2acb1b867f5536582f
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-25 12:13:10 +0000

    set functionality for LogOut on NavBar

--- src/components/Bars/NavBar.jsx
+++ src/components/Bars/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useContext } from "react";
 import logo from "../images/logo.png"
+import { AuthContext } from "../../context/auth.context";
 
 function NavBar() {
+
+    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+
     return (
         <>
             <div>
@@ -38,6 +43,18 @@ function NavBar() {
                 <NavLink to="/auth/signup">
                     Sign Up
                 </NavLink>
+
+                {isLoggedIn && (
+                    <>
+                    {/* no futuro é suposto ser um link para os eventos que são criados pelo do user, ou para o user profile*/}
+                        <Link to="/api/events">
+                            <button>{user.name}'s Events</button>
+                        </Link>
+
+                        <button onClick={logOutUser}>Logout</button>
+                    </>
+                )}
+
             </div>
         </>
     )
