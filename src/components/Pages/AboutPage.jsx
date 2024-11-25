4ef01e66c9fe414baeec03c7720e2591377b3755
Claudia Pando <claudiamfp@hotmail.com>
2024-11-25 11:25:48 +0000

    updated ActivityCard and Homepage, added NotFoundPage and AboutPage

--- src/components/Pages/AboutPage.jsx
+++ src/components/Pages/AboutPage.jsx
@@ -1,8 +1,23 @@
-function AboutPage(){
-<div>
-    
-</div>
-}
+function AboutPage() {
+    return(
+    <div>
+        <h1>Welcome to OutInPortugal – your go-to platform for discovering and sharing outdoor activities across Portugal!</h1>
+
+        <h2>Whether you're seeking an adventure in the mountains, a relaxing day at the beach, or a cultural walking tour, OutInPortugal connects you with an active community of outdoor enthusiasts.</h2>
+
+        <p>
+            Our mission is to inspire exploration, foster connections, and celebrate the beauty of Portugal's incredible landscapes – one activity at a time.
+
+            Whether you're a local or just visiting, OuInPortugal helps you make the most of your time outdoors. Together, let’s build a vibrant, active community that thrives in the fresh air and breathtaking scenery of Portugal.
+
+            Join us today and start your next adventure!
+        </p>
+
+        <h3>About the Team</h3>
+
+        <p>We are two Portuguese bootcampers, Claudia & Mariana currently building this platform from the ground up, putting into practice the skills we are acquiring throughout our bootcamp journey.</p>
+    </div>
+)}
 
 
 export default AboutPage;
