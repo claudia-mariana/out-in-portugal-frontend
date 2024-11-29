a452e280d964abb22abce1d9ecb794e6694c8d07
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-29 08:03:15 +0000

    implemented responsiveness for activity and event details pages

--- src/components/Pages/ActivityDetailsPage.jsx
+++ src/components/Pages/ActivityDetailsPage.jsx
@@ -27,45 +27,43 @@ function ActivityDetailsPage() {
     <div>
       <div>
         <NavLink to={"/api/activities"}>
-          <ArrowLeftIcon className="absolute w-[1.5vw] m-[2vw] text-blue" />
+          <ArrowLeftIcon className="absolute w-[5vw] m-[2vw] text-blue" />
         </NavLink>
       </div>
-
+  
       {activity ? (
         <>
           <div className="flex justify-center">
-            <div className="flex flex-col items-center bg-white w-[70vw] m-[2vw] shadow-md">
-                <div className="relative w-full">
-                  <img
-                    src={activity.imageUrl}
-                    alt={activity.title}
-                    className="w-full h-[70vh] object-cover rounded-t-md"
-                  />
-                  <div className="relative left-3 p-2 rounded-sm my-5">
-                    <h3 className="font-bold text-4xl">{activity.title}</h3>
-                  </div>
+            <div className="flex flex-col items-center bg-white w-[90vw] md:w-[70vw] m-[2vw] shadow-md">
+              <div className="relative w-full">
+                <img
+                  src={activity.imageUrl}
+                  alt={activity.title}
+                  className="w-full h-[40vh] md:h-[70vh] object-cover rounded-t-md"
+                />
+                <div className="relative left-3 p-2 rounded-sm my-5">
+                  <h3 className="font-bold text-4xl">{activity.title}</h3>
                 </div>
+              </div>
               <div className="relative flex flex-col w-full left-3 ">
-
                 <div className="whitespace-no-wrap my-5">
-                  <p className="text-black text-base font-semibold inline-block m-2">
+                  <p className="text-black text-xl font-semibold inline-block m-2">
                     Category:
                   </p>
-                  <span className="text-black bg-yellow text-base px-1 py-1 rounded-md">
+                  <span className="text-black bg-yellow text-xl px-1 py-1 rounded-md">
                     {activity.category}
                   </span>
-
+  
                   <div className="whitespace-no-wrap my-5">
-                    <p className="text-black text-base font-semibold m-2">
+                    <p className="text-black text-xl font-semibold m-2">
                       Description:
                     </p>
                     <p className="text-black text-base m-2">
                       {activity.description}
                     </p>
                   </div>
-
                 </div>
-
+  
                 <div className="whitespace-no-wrap ">
                   <h4 className="text-black text-xl font-semibold ml-2 mb-5">
                     Events:
@@ -74,14 +72,13 @@ function ActivityDetailsPage() {
                 <div className="flex-grow whitespace-nowrap">
                   {activity.events && activity.events.length > 0 ? (
                     activity.events.map((event) => (
-                      <div
-                        key={event._id}
-                        className="whitespace-no-wrap">
+                      <div key={event._id} className="whitespace-no-wrap">
                         <NavLink
-                          to={`/api/events/${event._id}`} className="block w-full md:w-auto p-2 bg-gray-light text-blue  hover:text-opacity-70 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+                          to={`/api/events/${event._id}`}
+                          className="block w-full md:w-auto p-2 bg-gray-light text-blue  hover:text-opacity-70 rounded-lg shadow-md hover:shadow-lg transition-shadow"
                         >
                           <h4 className="m-2 font-bold text-l">{event.title}</h4>
-
+  
                           <p className="text-black m-2">{event.description}</p>
                           <p className="text-black m-2">Date: {event.startDate}</p>
                         </NavLink>
@@ -94,8 +91,9 @@ function ActivityDetailsPage() {
                   )}
                   <div className="flex mt-10 mb-20">
                     <NavLink to={"/api/events/create"}>
-                      <button className="bg-blue center text-white text-sm py-2 px-4 rounded-md shadow-md hover:transform  hover:text-yellow"> 
-                        Create Event </button>
+                      <button className="bg-blue center text-white text-sm py-2 px-4 rounded-md shadow-md hover:transform  hover:text-yellow">
+                        Create Event
+                      </button>
                     </NavLink>
                   </div>
                 </div>
@@ -105,12 +103,10 @@ function ActivityDetailsPage() {
         </>
       ) : (
         <div className="flex justify-center items-center h-screen">
-            <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
-            <p className="ml-4">Loading...</p>
-          </div>
+          <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+          <p className="ml-4">Loading...</p>
+        </div>
       )}
-
-
     </div>
   );
 }
--- src/components/Pages/EventDetailsPage.jsx
+++ src/components/Pages/EventDetailsPage.jsx
@@ -1,50 +1,50 @@
-import { NavLink, useParams } from "react-router-dom"; 
-import eventsService from "../../services/events.service"; 
-import activitiesService from "../../services/activities.service"; 
-import { useState, useEffect } from "react"; 
-import { useNavigate } from 'react-router-dom'; 
-import { format } from 'date-fns'; 
+import { NavLink, useParams } from "react-router-dom";
+import eventsService from "../../services/events.service";
+import activitiesService from "../../services/activities.service";
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { format } from 'date-fns';
 import { ArrowLeftIcon, MapPinIcon, TrashIcon } from '@heroicons/react/24/outline'
 import { jwtDecode } from "jwt-decode";
 
 
 function EventDetailsPage() {
-    const navigate = useNavigate(); 
-    const [isAuthenticated, setIsAuthenticated] = useState(false); 
-    const { eventId } = useParams(); 
-    const [event, setEvent] = useState(null); 
-    const { activityId } = useState(null); 
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { eventId } = useParams();
+    const [event, setEvent] = useState(null);
+    const { activityId } = useState(null);
     const [activity, setActivity] = useState(null);
 
     const getUserIdFromToken = () => {
         const token = localStorage.getItem('authToken');
         if (!token) return null;
-    
-        try {
-          const decoded = jwtDecode(token);
-          return decoded._id; // Assuming the user ID is `_id` in the token
-        } catch (error) {
-          console.error("Invalid token", error);
-          return null;
-        }
-      };
-    
-      const userId = getUserIdFromToken();
 
-      useEffect(() => {
+        try {
+            const decoded = jwtDecode(token);
+            return decoded._id; // Assuming the user ID is `_id` in the token
+        } catch (error) {
+            console.error("Invalid token", error);
+            return null;
+        }
+    };
+
+    const userId = getUserIdFromToken();
+
+    useEffect(() => {
         const fetchEvent = async () => {
-          try {
-            const response = await eventsService.getEvent(eventId);
-            setEvent(response.data);
-          } catch (err) {
-            console.log(err);
-          }
+            try {
+                const response = await eventsService.getEvent(eventId);
+                setEvent(response.data);
+            } catch (err) {
+                console.log(err);
+            }
         };
-    
+
         fetchEvent();
-      }, [eventId]);
-    
-      const isOwner = event && event.createdBy === userId;    
+    }, [eventId]);
+
+    const isOwner = event && event.createdBy === userId;
 
 
 
@@ -53,130 +53,127 @@ function EventDetailsPage() {
         <div>
             <div>
                 <NavLink to={"/api/events"}>
-                    <ArrowLeftIcon className="absolute w-[1.5vw] m-[2vw] text-blue" />
+                    <ArrowLeftIcon className="absolute w-6 m-2 text-blue" />
                 </NavLink>
             </div>
 
-            {event ?
+            {event ? (
                 <>
                     <div className="flex flex-col items-center">
-
-                        <div className="flex flex-col bg-gray-light w-[70vw] m-[2vw] shadow-lg rounded-md relative">
-
-
-                            {/* Only show the delete button if authenticated
-                            {isAuthenticated && (
-                                <button
-                                    onClick={deleteEvent}
-                                    className="absolute top-0 right-0 m-[1vw] cursor-pointer pointer-events-auto z-50"
-                                >
-                                    <TrashIcon className="w-[1vw]" />
-                                </button>
-                            )} */}
-
+                        <div className="flex flex-col bg-gray-light w-full sm:w-[90vw] lg:w-[70vw] m-4 shadow-lg rounded-md relative">
                             <div className="relative p-4 rounded-sm mt-5 ml-2">
-                                <h3 className="font-bold text-4xl">
-                                    {event.title}</h3>
+                                <h3 className="font-bold text-3xl sm:text-4xl">{event.title}</h3>
                             </div>
+
                             <div className="whitespace-no-wrap p-4">
                                 <p className="text-black text-base font-semibold inline-block mx-2 mb-10 ml-2">
-                                    Activity:</p>
-                                {event && event.activity &&
+                                    Activity:
+                                </p>
+                                {event && event.activity && (
                                     <NavLink to={`/api/activities/${event.activity._id}`}>
-                                        <p className="text-black text-base inline-block m-2 transition-transform transform hover:scale-110 hover:text-shadow-lg">{event.activity.title}</p>
+                                        <p className="text-black text-base inline-block m-2 transition-transform transform hover:scale-110 hover:text-shadow-lg">
+                                            {event.activity.title}
+                                        </p>
                                     </NavLink>
-                                }
+                                )}
                             </div>
-                            <div className="flex gap-x-4">
-                                <div className="w-1/2 p-4 ">
+
+                            <div className="flex flex-col sm:flex-row gap-4">
+                                <div className="sm:w-1/2 p-4">
                                     <div className="whitespace-no-wrap my-5">
                                         <p className="text-black text-base font-semibold ml-2">
                                             Meeting Point:
                                         </p>
-                                        <MapPinIcon className=" w-[1.5vw] text-orange inline-block ml-2 font-extrabold" />
+                                        <MapPinIcon className="w-6 text-orange inline-block ml-2 font-extrabold" />
                                         <p className="text-black text-base m-2 inline-block">
                                             {event.meetingPoint}
                                         </p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Description:</p>
-                                        <p className="text-black text-base m-2">
-                                            {event.description}</p>
+                                            Description:
+                                        </p>
+                                        <p className="text-black text-base m-2">{event.description}</p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Hosted by:</p>
+                                            Hosted by:
+                                        </p>
                                         <p className="text-black text-base m-2">
-                                            {event.organization}</p>
+                                            {event.organization}
+                                        </p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Recommended for: </p>
+                                            Recommended for:{" "}
+                                        </p>
                                         <p className="text-black text-base ml-2">
-                                            {event.targetAudience}</p>
+                                            {event.targetAudience}
+                                        </p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold inline-block ml-2">
-                                            Please bring with you the following equipment: </p>
-                                        <p className="text-black text-base ml-2">
-                                            {event.equipment}</p>
+                                            Please bring with you the following equipment:{" "}
+                                        </p>
+                                        <p className="text-black text-base ml-2">{event.equipment}</p>
                                     </div>
-
                                 </div>
-                                <div className="w-1/2 bg-blue-200 p-4">
-                                    <div className="whitespace-no-wrap my-7 ">
+                                <div className="sm:w-1/2 bg-blue-200 p-4">
+                                    <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Price:</p>
-                                        <p className="text-black text-base ml-2 ">
-                                            {event.price}</p>
+                                            Price:
+                                        </p>
+                                        <p className="text-black text-base ml-2 ">{event.price}</p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Start Date: </p>
+                                            Start Date:{" "}
+                                        </p>
                                         <p className="text-black text-base ml-2 ">
-                                            {format(new Date(event.startDate), 'MMM do, yyyy')}</p>
+                                            {format(new Date(event.startDate), "MMM do, yyyy")}
+                                        </p>
                                         <p className="text-black text-base ml-2 ">
-                                            {format(new Date(event.startDate), 'HH:mm')}h</p>
+                                            {format(new Date(event.startDate), "HH:mm")}h
+                                        </p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            End Date: </p>
+                                            End Date:
+                                        </p>
                                         <p className="text-black text-base ml-2">
-                                            {format(new Date(event.endDate), 'MMM do, yyyy')} </p>
+                                            {format(new Date(event.endDate), "MMM do, yyyy")}
+                                        </p>
                                         <p className="text-black text-base ml-2">
-                                            {format(new Date(event.endDate), 'HH:mm')}h </p>
+                                            {format(new Date(event.endDate), "HH:mm")}h
+                                        </p>
                                     </div>
                                     <div className="whitespace-no-wrap my-7">
                                         <p className="text-black text-base font-semibold ml-2">
-                                            Duration: </p>
-                                        <p className="text-black text-base ml-2">
-                                            {event.duration}</p>
+                                            Duration:
+                                        </p>
+                                        <p className="text-black text-base ml-2">{event.duration}</p>
                                     </div>
-
                                 </div>
                             </div>
                             {isOwner && (
-              <div className="flex flex-row justify-center m-5">
-                <NavLink to={`/api/events/edit/${eventId}`}>
-                  <button className="bg-blue text-white py-2 px-4 rounded-md shadow-md hover:transform hover:shadow-lg hover:text-yellow">
-                    Edit Event
-                  </button>
-                </NavLink>
-              </div>
-            )}
+                                <div className="flex flex-row justify-center m-5">
+                                    <NavLink to={`/api/events/edit/${event._id}`}>
+                                        <button className="bg-blue text-white py-2 px-4 rounded-md shadow-md hover:transform hover:shadow-lg hover:text-yellow">
+                                            Edit Event
+                                        </button>
+                                    </NavLink>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </>
-                :
+            ) : (
                 <div className="flex justify-center items-center h-screen">
                     <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
                     <p className="ml-4">Loading...</p>
                 </div>
-            }
-
-
-        </div >
+            )}
+        </div>
     );
 
 }
