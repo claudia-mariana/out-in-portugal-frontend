a2863309996f4c89bc93700ba449418b26d372b3
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-29 05:42:12 +0000

    finished navBar responsiveness

--- src/components/Bars/NavBar.jsx
+++ src/components/Bars/NavBar.jsx
@@ -11,12 +11,18 @@ function NavBar() {
     setMenuOpen(!menuOpen);
   };
 
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
+
+
   return (
     <div className="bg-blue text-white shadow-md">
       <div className="flex items-center justify-between p-2 md:text-xl">
         {/* Logo and App Name */}
         <div className="flex items-center hover:text-yellow">
-          <NavLink to="/" className="flex items-center space-x-2">
+          <NavLink to="/" className="flex items-center space-x-2" onClick={handleMenuClose}>
             <img src={logo} alt="OutInPortugalLogo" className="h-12 w-auto" />
             <h3 className="text-lg font-semibold">OutInPortugal</h3>
           </NavLink>
@@ -40,44 +46,50 @@ function NavBar() {
           </svg>
         </div>
 
-        {/* Nav Links and User Actions combined for small screens */}
-        <div className={`md:flex md:justify-center md:items-center md:space-x-6 ${menuOpen ? "absolute top-16 inset-x-0 bg-blue flex flex-col items-center" : "hidden"} md:static md:flex-row`}>
-          <NavLink className="hover:text-yellow px-3" to="/api/activities">
-            Activities
-          </NavLink>
-          <NavLink className="hover:text-yellow px-3" to="/api/events">
-            Events
-          </NavLink>
-          <NavLink className="hover:text-yellow px-3" to="/api/events/create">
-            Create Event
-          </NavLink>
-          <NavLink className="hover:text-yellow px-3" to="/about-page">
-            About
-          </NavLink>
+        <div className={`absolute md:static md:flex md:items-center ${menuOpen ? "z-50 top-16 inset-x-0 bg-blue flex flex-col items-center" : "hidden"} md:flex`}>
+          {/* Main Navigation Links for small screens */}
+          <div className={`flex flex-col md:flex-row md:absolute md:left-1/2 md:transform md:-translate-x-1/2 md:space-x-6 items-center ${menuOpen ? 'space-y-2' : ''}`}>
+            <NavLink className="hover:text-yellow px-3" to="/api/activities" onClick={handleMenuClose}>
+              Activities
+            </NavLink>
+            <NavLink className="hover:text-yellow px-3" to="/api/events" onClick={handleMenuClose}>
+              Events
+            </NavLink>
+            <NavLink className="hover:text-yellow px-3" to="/api/events/create" onClick={handleMenuClose}>
+              Create Event
+            </NavLink>
+            <NavLink className="hover:text-yellow px-3" to="/about-page" onClick={handleMenuClose}>
+              About
+            </NavLink>
+          </div>
 
           {/* User Actions inside collapsible menu for small screens */}
-          <div className="flex flex-col items-center space-y-2 mt-4 md:mt-0 md:flex-row md:space-x-4 md:space-y-0">
+          <div className={`flex flex-col items-center space-y-2 mt-4 md:mt-0 md:flex-row md:space-x-4 md:space-y-0 ${menuOpen ? 'w-full items-center' : ''}`}>
             {!isLoggedIn ? (
               <>
-                <NavLink className="hover:text-yellow" to="/auth/login">
+                <NavLink className="hover:text-yellow" to="/auth/login" onClick={handleMenuClose}>
                   Log In
                 </NavLink>
                 <NavLink
                   className="bg-blue-medium text-white px-4 py-2 rounded-md hover:text-yellow transition-colors"
                   to="/auth/signup"
+                  onClick={handleMenuClose}
                 >
                   Sign Up
                 </NavLink>
               </>
             ) : (
               <>
-                <Link to="/api/events">
+                <Link to="/api/events" onClick={handleMenuClose}>
                   <button className="hover:text-yellow p-2 rounded">
                     {user.name}
                   </button>
                 </Link>
                 <button
-                  onClick={logOutUser}
+                  onClick={() => {
+                    logOutUser();
+                    handleMenuClose();
+                  }}
                   className="bg-blue-medium text-white px-4 py-2 rounded-md hover:text-yellow transition-colors"
                 >
                   Log Out
